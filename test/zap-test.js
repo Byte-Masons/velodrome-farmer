b872c73c4df43ca89526904171e4760a7349ec84
tess3rac7 <tess3rac7@protonmail.com>
2022-07-28 16:03:54 -0400

    alETH/WETH tests working.

--- test/zap-test.js
+++ test/zap-test.js
@@ -34,114 +34,154 @@ describe('Zapper', function () {
     await zapper.deployed();
   });
 
-  it('alETH-WETH sAMM', async function () {
+  it('alETH-WETH sAMM estimate swaps', async () => {
     const cryptAddr = '0xD268887B2171c4b7595DeeBD0CB589c560682629';
-    const alETHAddr = '0x3E29D3A9316dAB217754d13b28646B76607c5f04';
+    const token1Addr = wethAddr;
+    const token2Addr = '0x3E29D3A9316dAB217754d13b28646B76607c5f04'; // alETH
+
+    const token1 = await Token.attach(token1Addr);
+    const token2 = await Token.attach(token2Addr);
+
+    const token1Symbol = await token1.symbol();
+    const token2Symbol = await token2.symbol();
+
+    // estimate swaps
+    await estimateSwaps(cryptAddr, token1Addr, token1Symbol, token2Symbol, [
+      hre.ethers.utils.parseEther('1'),
+      hre.ethers.utils.parseEther('3'),
+      hre.ethers.utils.parseEther('5'),
+      hre.ethers.utils.parseEther('10'),
+      hre.ethers.utils.parseEther('50'),
+    ]);
+    await estimateSwaps(cryptAddr, token2Addr, token2Symbol, token1Symbol, [
+      hre.ethers.utils.parseEther('1'),
+      hre.ethers.utils.parseEther('3'),
+      hre.ethers.utils.parseEther('5'),
+      hre.ethers.utils.parseEther('10'),
+      hre.ethers.utils.parseEther('50'),
+    ]);
+  });
+
+  it('alETH-WETH sAMM zap in and out', async () => {
+    const cryptAddr = '0xD268887B2171c4b7595DeeBD0CB589c560682629';
+    const token1Addr = wethAddr;
+    const token2Addr = '0x3E29D3A9316dAB217754d13b28646B76607c5f04'; // alETH
 
     const crypt = await Crypt.attach(cryptAddr);
     const lp = await Token.attach('0x6fD5BEe1Ddb4dbBB0b7368B080Ab99b8BA765902');
-    const weth = await Token.attach(wethAddr);
-    const alEth = await Token.attach(alETHAddr);
-    let tokenAmountOutMinAlEth, tokenAmountOutMinWeth;
-
-    // estimate swaps
-    // 1 WETH
-    let estimateOutput = await zapper.estimateSwap(cryptAddr, wethAddr, hre.ethers.utils.parseEther('1'));
-    let swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    let swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 1 WETH.. ${swapAmountIn} WETH will be swapped to produce ${swapAmountOut} alETH`);
-
-    // 3 WETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, wethAddr, hre.ethers.utils.parseEther('3'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 3 WETH.. ${swapAmountIn} WETH will be swapped to produce ${swapAmountOut} alETH`);
-
-    // 5 WETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, wethAddr, hre.ethers.utils.parseEther('5'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    tokenAmountOutMinAlEth = estimateOutput.swapAmountOut;
-    console.log(`Sending 5 WETH.. ${swapAmountIn} WETH will be swapped to produce ${swapAmountOut} alETH`);
-
-    // 10 WETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, wethAddr, hre.ethers.utils.parseEther('10'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 10 WETH.. ${swapAmountIn} WETH will be swapped to produce ${swapAmountOut} alETH`);
-
-    // 50 WETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, wethAddr, hre.ethers.utils.parseEther('50'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 50 WETH.. ${swapAmountIn} WETH will be swapped to produce ${swapAmountOut} alETH`);
-
-    // 1 alETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, alETHAddr, hre.ethers.utils.parseEther('1'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 1 alETH.. ${swapAmountIn} alETH will be swapped to produce ${swapAmountOut} WETH`);
-
-    // 3 alETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, alETHAddr, hre.ethers.utils.parseEther('3'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 3 alETH.. ${swapAmountIn} alETH will be swapped to produce ${swapAmountOut} WETH`);
-
-    // 5 alETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, alETHAddr, hre.ethers.utils.parseEther('5'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    tokenAmountOutMinWeth = estimateOutput.swapAmountOut;
-    console.log(`Sending 5 alETH.. ${swapAmountIn} alETH will be swapped to produce ${swapAmountOut} WETH`);
-
-    // 10 alETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, alETHAddr, hre.ethers.utils.parseEther('10'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 10 alETH.. ${swapAmountIn} alETH will be swapped to produce ${swapAmountOut} WETH`);
-
-    // 50 alETH
-    estimateOutput = await zapper.estimateSwap(cryptAddr, alETHAddr, hre.ethers.utils.parseEther('50'));
-    swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
-    swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
-    console.log(`Sending 50 alETH.. ${swapAmountIn} alETH will be swapped to produce ${swapAmountOut} WETH`);
+    const token1 = await Token.attach(token1Addr);
+    const token2 = await Token.attach(token2Addr);
 
     // zapIn with 5 WETH
-    let tokenHolderAddr = '0x68526A4295236D2f18cEda8A200CdDD5Aab9e2cC';
+    const tokenHolderAddr = '0x68526A4295236D2f18cEda8A200CdDD5Aab9e2cC';
     await hre.network.provider.request({
       method: 'hardhat_impersonateAccount',
       params: [tokenHolderAddr],
     });
-    let tokenHolder = await hre.ethers.provider.getSigner(tokenHolderAddr);
+    const tokenHolder = await hre.ethers.provider.getSigner(tokenHolderAddr);
+    await zapIn(hre.ethers.utils.parseEther('5'), token1, token2, lp, tokenHolder, tokenHolderAddr, crypt, cryptAddr);
+    await zapOut(token1, token2, tokenHolder, tokenHolderAddr, crypt, cryptAddr);
+  });
 
-    console.log(`\n----------Zapping in with 5 WETH----------`);
-    await weth.connect(tokenHolder).approve(zapper.address, hre.ethers.constants.MaxUint256);
+  it('alETH-WETH sAMM zap in and out (with swapping to WETH)', async () => {
+    const cryptAddr = '0xD268887B2171c4b7595DeeBD0CB589c560682629';
+    const token1Addr = wethAddr;
+    const token2Addr = '0x3E29D3A9316dAB217754d13b28646B76607c5f04'; // alETH
+
+    const crypt = await Crypt.attach(cryptAddr);
+    const lp = await Token.attach('0x6fD5BEe1Ddb4dbBB0b7368B080Ab99b8BA765902');
+    const token1 = await Token.attach(token1Addr);
+    const token2 = await Token.attach(token2Addr);
+
+    // zapIn with 5 WETH
+    const tokenHolderAddr = '0x68526A4295236D2f18cEda8A200CdDD5Aab9e2cC';
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [tokenHolderAddr],
+    });
+    const tokenHolder = await hre.ethers.provider.getSigner(tokenHolderAddr);
+    await zapIn(hre.ethers.utils.parseEther('5'), token1, token2, lp, tokenHolder, tokenHolderAddr, crypt, cryptAddr);
+    await zapOut(token1, token2, tokenHolder, tokenHolderAddr, crypt, cryptAddr, token1);
+  });
+
+  it('alETH-WETH sAMM zap in and out (with swapping to alETH)', async () => {
+    const cryptAddr = '0xD268887B2171c4b7595DeeBD0CB589c560682629';
+    const token1Addr = wethAddr;
+    const token2Addr = '0x3E29D3A9316dAB217754d13b28646B76607c5f04'; // alETH
+
+    const crypt = await Crypt.attach(cryptAddr);
+    const lp = await Token.attach('0x6fD5BEe1Ddb4dbBB0b7368B080Ab99b8BA765902');
+    const token1 = await Token.attach(token1Addr);
+    const token2 = await Token.attach(token2Addr);
+
+    // zapIn with 5 WETH
+    const tokenHolderAddr = '0x68526A4295236D2f18cEda8A200CdDD5Aab9e2cC';
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [tokenHolderAddr],
+    });
+    const tokenHolder = await hre.ethers.provider.getSigner(tokenHolderAddr);
+    await zapIn(hre.ethers.utils.parseEther('5'), token1, token2, lp, tokenHolder, tokenHolderAddr, crypt, cryptAddr);
+    await zapOut(token1, token2, tokenHolder, tokenHolderAddr, crypt, cryptAddr, token2);
+  });
+
+  async function estimateSwaps(cryptAddr, token1Addr, token1Symbol, token2Symbol, amounts) {
+    for (let i = 0; i < amounts.length; i++) {
+      const estimateOutput = await zapper.estimateSwap(cryptAddr, token1Addr, amounts[i]);
+      const swapAmountIn = hre.ethers.utils.formatEther(estimateOutput.swapAmountIn);
+      const swapAmountOut = hre.ethers.utils.formatEther(estimateOutput.swapAmountOut);
+      console.log(
+        `Sending ${hre.ethers.utils.formatEther(
+          amounts[i],
+        )} ${token1Symbol}.. ${swapAmountIn} ${token1Symbol} will be swapped to produce ${swapAmountOut} ${token2Symbol}`,
+      );
+    }
+  }
+
+  async function zapIn(amount, tokenIn, otherToken, lpToken, tokenHolder, tokenHolderAddr, crypt, cryptAddr) {
+    const tokenInSymbol = await tokenIn.symbol();
+    const otherTokenSymbol = await otherToken.symbol();
+
+    const estimateOutput = await zapper.estimateSwap(cryptAddr, tokenIn.address, amount);
+    const swapAmountOut = estimateOutput.swapAmountOut;
+
+    console.log(`\n----------Zapping in with ${hre.ethers.utils.formatEther(amount)} ${tokenInSymbol}----------`);
+    await tokenIn.connect(tokenHolder).approve(zapper.address, hre.ethers.constants.MaxUint256);
     console.log(
       `Starting user ETH (native) balance = ${hre.ethers.utils.formatEther(
         await hre.ethers.provider.getBalance(tokenHolderAddr),
       )}`,
     );
-    console.log(`Starting user WETH balance = ${hre.ethers.utils.formatEther(await weth.balanceOf(tokenHolderAddr))}`);
     console.log(
-      `Starting user alETH balance = ${hre.ethers.utils.formatEther(await alEth.balanceOf(tokenHolderAddr))}`,
+      `Starting user ${tokenInSymbol} balance = ${hre.ethers.utils.formatEther(
+        await tokenIn.balanceOf(tokenHolderAddr),
+      )}`,
     );
-    console.log(`Starting user LP balance = ${hre.ethers.utils.formatEther(await lp.balanceOf(tokenHolderAddr))}`);
-    await zapper
-      .connect(tokenHolder)
-      .reapIn(cryptAddr, tokenAmountOutMinAlEth, wethAddr, hre.ethers.utils.parseEther('5'));
+    console.log(
+      `Starting user ${otherTokenSymbol} balance = ${hre.ethers.utils.formatEther(
+        await otherToken.balanceOf(tokenHolderAddr),
+      )}`,
+    );
+    console.log(`Starting user LP balance = ${hre.ethers.utils.formatEther(await lpToken.balanceOf(tokenHolderAddr))}`);
+    await zapper.connect(tokenHolder).reapIn(cryptAddr, swapAmountOut, wethAddr, amount);
     console.log(
       `\nPost-zap user ETH (native) balance = ${hre.ethers.utils.formatEther(
         await hre.ethers.provider.getBalance(tokenHolderAddr),
       )}`,
     );
-    console.log(`Post-zap user WETH balance = ${hre.ethers.utils.formatEther(await weth.balanceOf(tokenHolderAddr))}`);
     console.log(
-      `Post-zap user alETH balance = ${hre.ethers.utils.formatEther(await alEth.balanceOf(tokenHolderAddr))}`,
+      `Post-zap user ${tokenInSymbol} balance = ${hre.ethers.utils.formatEther(
+        await tokenIn.balanceOf(tokenHolderAddr),
+      )}`,
+    );
+    console.log(
+      `Post-zap user ${otherTokenSymbol} balance = ${hre.ethers.utils.formatEther(
+        await otherToken.balanceOf(tokenHolderAddr),
+      )}`,
     );
     let endingShareBal = await crypt.balanceOf(tokenHolderAddr);
     console.log(`Post-zap user crypt share balance = ${hre.ethers.utils.formatEther(endingShareBal)}`);
-    let ppfs = await crypt.getPricePerFullShare();
+    const ppfs = await crypt.getPricePerFullShare();
     let endingLPBal = ppfs.mul(endingShareBal).div(hre.ethers.utils.parseEther('1'));
     console.log(`Post-zap user LP balance (from ppfs) = ${hre.ethers.utils.formatEther(endingLPBal)}`);
 
@@ -150,41 +190,76 @@ describe('Zapper', function () {
         await hre.ethers.provider.getBalance(zapper.address),
       )}`,
     );
-    console.log(`Post-zap zapper WETH balance = ${hre.ethers.utils.formatEther(await weth.balanceOf(zapper.address))}`);
     console.log(
-      `Post-zap zapper alETH balance = ${hre.ethers.utils.formatEther(await alEth.balanceOf(zapper.address))}`,
+      `Post-zap zapper ${tokenInSymbol} balance = ${hre.ethers.utils.formatEther(
+        await tokenIn.balanceOf(zapper.address),
+      )}`,
+    );
+    console.log(
+      `Post-zap zapper ${otherTokenSymbol} balance = ${hre.ethers.utils.formatEther(
+        await otherToken.balanceOf(zapper.address),
+      )}`,
     );
     endingShareBal = await crypt.balanceOf(zapper.address);
     console.log(`Post-zap zapper crypt share balance = ${hre.ethers.utils.formatEther(endingShareBal)}`);
     endingLPBal = ppfs.mul(endingShareBal).div(hre.ethers.utils.parseEther('1'));
     console.log(`Post-zap zapper LP balance (from ppfs) = ${hre.ethers.utils.formatEther(endingLPBal)}`);
+  }
 
-    console.log(`\nZapping back out (without swapping)`);
+  async function zapOut(tokenIn, otherToken, tokenHolder, tokenHolderAddr, crypt, cryptAddr, desiredOutputToken) {
+    const tokenInSymbol = await tokenIn.symbol();
+    const otherTokenSymbol = await otherToken.symbol();
+
+    if (desiredOutputToken !== undefined) {
+      console.log(`\nZapping back out (swapping to ${await desiredOutputToken.symbol()})`);
+    } else {
+      console.log(`\nZapping back out (without swapping)`);
+    }
     await crypt.connect(tokenHolder).approve(zapper.address, hre.ethers.constants.MaxUint256);
-    await zapper.connect(tokenHolder).reapOut(cryptAddr, await crypt.balanceOf(tokenHolderAddr));
+    if (desiredOutputToken !== undefined) {
+      await zapper
+        .connect(tokenHolder)
+        .reapOutAndSwap(cryptAddr, await crypt.balanceOf(tokenHolderAddr), desiredOutputToken.address, 0);
+    } else {
+      await zapper.connect(tokenHolder).reapOut(cryptAddr, await crypt.balanceOf(tokenHolderAddr));
+    }
     console.log(
       `\nFinal user ETH (native) balance = ${hre.ethers.utils.formatEther(
         await hre.ethers.provider.getBalance(tokenHolderAddr),
       )}`,
     );
-    console.log(`Final user WETH balance = ${hre.ethers.utils.formatEther(await weth.balanceOf(tokenHolderAddr))}`);
-    console.log(`Final user alETH balance = ${hre.ethers.utils.formatEther(await alEth.balanceOf(tokenHolderAddr))}`);
-    endingShareBal = await crypt.balanceOf(tokenHolderAddr);
+    console.log(
+      `Final user ${tokenInSymbol} balance = ${hre.ethers.utils.formatEther(await tokenIn.balanceOf(tokenHolderAddr))}`,
+    );
+    console.log(
+      `Final user ${otherTokenSymbol} balance = ${hre.ethers.utils.formatEther(
+        await otherToken.balanceOf(tokenHolderAddr),
+      )}`,
+    );
+    let endingShareBal = await crypt.balanceOf(tokenHolderAddr);
     console.log(`Final user crypt share balance = ${hre.ethers.utils.formatEther(endingShareBal)}`);
-    ppfs = await crypt.getPricePerFullShare();
-    endingLPBal = ppfs.mul(endingShareBal).div(hre.ethers.utils.parseEther('1'));
+    const ppfs = await crypt.getPricePerFullShare();
+    let endingLPBal = ppfs.mul(endingShareBal).div(hre.ethers.utils.parseEther('1'));
     console.log(`Final user LP balance (from ppfs) = ${hre.ethers.utils.formatEther(endingLPBal)}`);
 
     console.log(
-      `\nFinal zapper ETH (native) balance = ${hre.ethers.utils.formatEther(
+      `Final zapper ETH (native) balance = ${hre.ethers.utils.formatEther(
         await hre.ethers.provider.getBalance(zapper.address),
       )}`,
     );
-    console.log(`Final zapper WETH balance = ${hre.ethers.utils.formatEther(await weth.balanceOf(zapper.address))}`);
-    console.log(`Final zapper alETH balance = ${hre.ethers.utils.formatEther(await alEth.balanceOf(zapper.address))}`);
+    console.log(
+      `Final zapper ${tokenInSymbol} balance = ${hre.ethers.utils.formatEther(
+        await tokenIn.balanceOf(zapper.address),
+      )}`,
+    );
+    console.log(
+      `Final zapper ${otherTokenSymbol} balance = ${hre.ethers.utils.formatEther(
+        await otherToken.balanceOf(zapper.address),
+      )}`,
+    );
     endingShareBal = await crypt.balanceOf(zapper.address);
     console.log(`Final zapper crypt share balance = ${hre.ethers.utils.formatEther(endingShareBal)}`);
     endingLPBal = ppfs.mul(endingShareBal).div(hre.ethers.utils.parseEther('1'));
     console.log(`Final zapper LP balance (from ppfs) = ${hre.ethers.utils.formatEther(endingLPBal)}`);
-  });
+  }
 });
