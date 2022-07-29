6c5f2dc9b1abe24f0fe0c5cca6501af01d4c44cb
tess3rac7 <tess3rac7@protonmail.com>
2022-07-28 23:20:10 -0400

    Zapper deployed.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,13 +1,16 @@
 async function main() {
-  const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
+  const Vault = await ethers.getContractFactory('ReaperVeloZap');
 
-  const wantAddress = '';
-  const tokenName = '';
-  const tokenSymbol = '';
-  const depositFee = 0;
-  const tvlCap = ethers.constants.MaxUint256;
+  // const wantAddress = '';
+  // const tokenName = '';
+  // const tokenSymbol = '';
+  // const depositFee = 0;
+  // const tvlCap = ethers.constants.MaxUint256;
 
-  const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap);
+  const vault = await Vault.deploy(
+    '0xa132DAB612dB5cB9fC9Ac426A0Cc215A3423F9c9',
+    '0x4200000000000000000000000000000000000006',
+  );
 
   await vault.deployed();
   console.log('Vault deployed to:', vault.address);
