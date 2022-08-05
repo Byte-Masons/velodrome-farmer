eb21f405df42b242b179c62c85f1a981c78b8e71
Bastien Guyonnet <bastien.guyonnet@tutanota.com>
2022-08-05 14:04:47 +0200

    updgraded

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,6 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperStrategyHappyRoad');
-  const stratContract = await hre.upgrades.upgradeProxy('0x8fA82f61153E7746112Cb4e9de1033504C3294eE', stratFactory);
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyVelodromeUsdcStable');
+  const stratContract = await hre.upgrades.upgradeProxy('0x8B6e31f3286b4D0B611Ff326E9682651A893aD70', stratFactory);
   console.log('Strategy upgraded!');
   // const stratContract = await stratFactory.attach('0x0310b9979BcC17fa2DB4cEC4417FCebabc405F1D');
 }
